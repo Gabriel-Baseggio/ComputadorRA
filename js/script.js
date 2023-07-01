ecf392f9861daf4d5be29602f62d27a2a6d17f2d
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-06-30 21:51:45 -0300

    Computador RA

--- js/script.js
+++ js/script.js
@@ -6,4 +6,21 @@ document.getElementById("target1").addEventListener("targetLost", () => {
     document.getElementsByClassName("placaMaeCard")[0].style.display = "none";
 })
 
-// document.getElementById("placaMaeCard")
+function nMostrar() {
+    document.getElementsByClassName("placaMaeCard")[0].style.display = "none";
+}
+
+function mostrar() {
+    document.getElementsByClassName("placaMaeCard")[0].style.display = "block";
+}
+
+let x = 0;
+document.getElementsByClassName("placaMaeCard")[0].addEventListener('click', () => {
+    let placaMaeCard = document.getElementsByClassName("placaMaeCard")[0];
+    if (x % 2 == 0) {
+        placaMaeCard.style.transform = "rotateY(180deg)";
+    } else {
+        placaMaeCard.style.transform = "rotateY(0deg)";
+    }
+    x++;
+})
