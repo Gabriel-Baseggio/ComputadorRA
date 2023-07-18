e8a41f7ffabf8a8b414ecdc5a4b058413ce94e4c
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-07-18 18:40:59 -0300

    Computador RA

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ document.getElementById("pcAbertoTarget").addEventListener("targetFound", () => 
 })
 
 document.getElementById("pcAbertoTarget").addEventListener("targetLost", () => {
+    noneEmTudo();
     document.getElementById("botao-aberto").style.display = "none";
 })
 
